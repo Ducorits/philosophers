9cf43845e25b402aaf83a6eec482d1ea7b32940c
Duco Ritsema van Eck <ducorits@zonderstreepje.nl>
2023-04-03 16:28:10 +0200

    quick fix for time to die 0

--- philo/src/init.c
+++ philo/src/init.c
@@ -6,7 +6,7 @@
 /*   By: dritsema <dritsema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/11 21:45:29 by dritsema      #+#    #+#                 */
-/*   Updated: 2023/04/03 14:42:49 by dritsema      ########   odam.nl         */
+/*   Updated: 2023/04/03 16:26:48 by dritsema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,7 @@ t_info	*init(int argc, char **argv)
 	else
 		info->eat_goal = -1;
 	info->time_to_think = (info->time_to_eat * 2) - info->time_to_sleep;
-	if (info->philo_count <= 0)
+	if (info->philo_count <= 0 || info->time_to_die <= 0)
 		return (free(info), NULL);
 	if (!init_mutexes(info))
 		return (NULL);
